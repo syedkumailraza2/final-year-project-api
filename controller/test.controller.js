e9ffd64eee16d1669f6eacedcd83517da81d0107
zaidyargatti <9928zaid@gamil.com>
2025-04-02 16:38:45 +0530

    Delete controller added

--- controller/test.controller.js
+++ controller/test.controller.js
@@ -1,5 +1,6 @@
 import Test from "../model/test.model.js";
 
+
 const createTest = async(req,res)=>{
     try {
         const {title,date,time,duration,question1,question2,question3,question4,question5} =req.body
@@ -27,6 +28,24 @@ const createTest = async(req,res)=>{
     }
 }
 
+const deleteTest = async(req,res)=>{
+    try {
+        const {id}=req.params
+        const test = await Test.findById(id)
+        if (!test) {
+            return res.status(404).json({ message: "Test not found" });
+        }
+
+        const deletedTest = await test.deleteOne()
+
+        res.status(200).json({deletedTest})
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
 export{
-    createTest
+    createTest,
+    deleteTest
 }
--- router/test.routes.js
+++ router/test.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createTest } from "../controller/test.controller.js";
+import { createTest, deleteTest } from "../controller/test.controller.js";
 import protect from "../middleware/jwt.middleware.js";
+import Test from "../model/test.model.js";
 
 
 const Testpath=Router()
 
 Testpath.post('/',protect,createTest)
-
+Testpath.delete('/:id',protect,deleteTest)
 export default Testpath
