1f6880796413d91252473673ee87bda6a99f37a4
Shadab-2604 <138616478+Shadab-2604@users.noreply.github.com>
2025-04-11 12:09:10 +0530

    update index

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
+
 // Static files configuration
 const uploadsDir = path.join(__dirname, "public", "uploads");
 app.use("/uploads", express.static(uploadsDir, {
